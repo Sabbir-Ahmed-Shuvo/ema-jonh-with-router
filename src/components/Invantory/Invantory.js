ee85afc7552db4e24d1465d9a76e238ac9d15b7a
sabbirNew <sabbir.ahmed5936@gmail.com>
2021-10-01 19:16:42 +0600

    updated all file

---
+++ src/components/Invantory/Invantory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invantory = () => {
+    return (
+        <div>
+            <h1>this is Invantory</h1>
+        </div>
+    );
+};
+
+export default Invantory;
